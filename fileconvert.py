d2ec5f4e13969a328e45d82ee42d230f419830de
coder-guy1106 <107420892+coder-guy1106@users.noreply.github.com>
2026-05-21 14:28:07 -0600

    Update fileconvert.py

--- fileconvert.py
+++ fileconvert.py
@@ -2,14 +2,10 @@ from tkinter import filedialog as fd
 from PIL import Image,ImageTk
 import tkinter as tk
 root = tk.Tk()
-
 root.title("file converter")
-
-
-
-
-
-
+#lab = tk.Label(root)
+#lab.pack(fill='both',padx=10,pady=10)
+supported=['png','jpg','jpeg','webp','bmp','tif','tiff','ico','icns','ppm','pgm','pbm','tga','eps']
 
 def select_file():
     filetypes = (
@@ -21,9 +17,11 @@ def select_file():
     print("\n------------------------------------\n")
 
     im= Image.open(f)
+    w=300
+    im.thumbnail((w,w))
     im_label = ImageTk.PhotoImage(im)
     lab = tk.Label(root, image=im_label)
-    lab.pack(side="left",expand=True)
+    lab.pack(fill='both',padx=10,pady=10)
     root.mainloop()
 
 
@@ -32,18 +30,33 @@ button1 = tk.Button(
     root,
     text='Open a File',
     command=select_file,
-    bg="azure4"   
+    bg="azure4"
+    height=1
+    font=("arial",25,"bold")
 )
-button1.pack(side="top",fill="x",expand=True)
-supported=['png','jpg','jpeg','webp','bmp','tif','tiff','ico','icns','ppm','pgm','pbm','tga','eps']
+
+button1.pack(side="top",fill="x")
+
+
+# RIGHT UI
+right_ui = tk.Frame(root)
+right_ui.pack(side="right")
+
+filetype = tk.Label(text="No file selected")
+filetype.pack(side="top")
+
 
 list = tk.Listbox(root)
 for i in range(1,15):
     list.insert(i,supported[i-1])
-
 list.pack(side="right")
 
 
+
+
+
+
+
 root.geometry("+"+str(860)+"+"+str(390))
-root.geometry("300x200")
+root.geometry("400x350")
 root.mainloop()
